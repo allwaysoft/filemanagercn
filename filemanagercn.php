7c1caa7765a930850cd5af9bcfdae6f076e916cc
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2020-10-24 10:56:22 +0800

    Add files via upload

--- filemanagercn.php
+++ filemanagercn.php
@@ -37,6 +37,31 @@ $iconv_input_encoding = 'CP1251';
 // date() format for file modification date
 $datetime_format = 'yy-m-d H:i';
 
+
+// Possible rules are 'OFF', 'AND' or 'OR'
+// OFF => Don't check connection IP, defaults to OFF
+// AND => Connection must be on the whitelist, and not on the blacklist
+// OR => Connection must be on the whitelist, or not on the blacklist
+$ip_ruleset = 'AND';
+
+// Should users be notified of their block?
+$ip_silent = true;
+
+// IP-addresses, both ipv4 and ipv6
+$ip_whitelist = array(
+    '127.0.0.1',    // local ipv4
+    '192.168.174.1',    // local ipv4
+    '::1'           // local ipv6
+);
+
+// IP-addresses, both ipv4 and ipv6
+$ip_blacklist = array(
+    '0.0.0.0',      // non-routable meta ipv4
+    '::'            // non-routable meta ipv6
+);
+
+
+
 //--- EDIT BELOW CAREFULLY OR DO NOT EDIT AT ALL
 
 // if fm included
@@ -92,6 +117,39 @@ if (isset($_GET['logout'])) {
 // Show image here
 if (isset($_GET['img'])) {
     fm_show_image($_GET['img']);
+}
+
+// Validate connection IP
+if($ip_ruleset != 'OFF'){
+    $clientIp = $_SERVER['REMOTE_ADDR'];
+
+    $proceed = false;
+
+    $whitelisted = in_array($clientIp, $ip_whitelist);
+    $blacklisted = in_array($clientIp, $ip_blacklist);
+
+    if($ip_ruleset == 'AND'){
+        if($whitelisted == true && $blacklisted == false){
+            $proceed = true;
+        }
+    } else
+    if($ip_ruleset == 'OR'){
+         if($whitelisted == true || $blacklisted == false){
+            $proceed = true;
+        }
+    }
+
+    if($proceed == false){
+        trigger_error('User connection denied from: ' . $clientIp, E_USER_WARNING);
+
+        if($ip_silent == false){
+            fm_set_msg('Access denied. IP restriction applicable', 'error');
+            fm_show_header_login();
+            fm_show_message();
+        }
+
+        exit();
+    }
 }
 
 // Auth
